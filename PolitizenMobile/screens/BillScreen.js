db28a4546d2e9416ed679e09019047eb652fe8af
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-02-01 00:34:10 -0500

    Frontend Update
    Added the articles to the application and the ability to navigate to them once pressing on the image or title. Put the buttons on the bill screen and changed the styling on the Home Screen to accommodate newer iPhone models. Added more assets and a folder for the article screens.

--- PolitizenMobile/screens/BillScreen.js
+++ PolitizenMobile/screens/BillScreen.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, ScrollView, Text } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
 export default function BillScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollVIew}>
-        <Text>Bill</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Local Legislation</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonTwo}>
+          <Text style={styles.buttonText}>State Legislation</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
@@ -21,5 +32,27 @@ const styles = StyleSheet.create({
 
   scrollView: {
     width: "100%",
+  },
+
+  button: {
+    backgroundColor: "#62787f",
+    height: 40,
+    width: 250,
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 130,
+  },
+
+  buttonTwo: {
+    backgroundColor: "#62787f",
+    height: 40,
+    width: 250,
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 40,
+  },
+
+  buttonText: {
+    color: "#fff",
   },
 });
