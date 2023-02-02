7be1a9746bbd644b94c7154e1db392b6a5a34305
Harry Patiry <60627616+harrypatiry@users.noreply.github.com>
2023-02-01 16:09:29 -0800

    candidate screen

--- PolitizenMobile/screens/CandidateScreen.js
+++ PolitizenMobile/screens/CandidateScreen.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, ScrollView, Text } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Text,
+  Image,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import TopNav from "../components/Header";
 
 export default function CandidateScreen() {
   return (
     <SafeAreaView style={styles.container}>
+      <TopNav />
       <ScrollView style={styles.scrollVIew}>
-        <Text>Yerrrr</Text>
+        <View>
+          <Image style={styles.candidateImg} source={require("../assets/candidates/CharlieCrist.jpg")}></Image>
+          <Text>Charlie Crist</Text>
+          <Image style={styles.candidateImg} source={require("../assets/candidates/KariLake.jpg")}></Image>
+          <Text>Kari Lake</Text>
+          <Image style={styles.candidateImg} source={require("../assets/candidates/KatieHobbs.jpeg")}></Image>
+          <Text>Katie Hobbs</Text>
+          <Image style={styles.candidateImg} source={require("../assets/candidates/RaphaelWarnock.jpg")}></Image>
+          <Text>Raphael Warnock</Text>
+          <Image style={styles.candidateImg} source={require("../assets/candidates/HerschelWalker.jpg")}></Image>
+          <Text>Herschel Walker</Text>
+          <Image style={styles.candidateImg} source={require("../assets/candidates/RonaldDeSantis.jpg")}></Image>
+          <Text>RonaldDe Santis</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -18,6 +41,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  articleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  headLogo: {
+    height: 109,
+    width: 110,
+  },
+
+  image: {
+    height: 70,
+    width: 70,
+    marginTop: 20,
+  },
+
+  candidateImg: {
+    marginTop: 50,
+    maxHeight: 250,
+    maxWidth: 250,
+    minWidth: 50,
+    minHeight: 50,
+  },
+
 
   scrollView: {
     width: "100%",
