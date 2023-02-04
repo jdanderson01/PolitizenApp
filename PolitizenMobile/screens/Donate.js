2a1f500f82c771e453eb37705de7ae2fa5539683
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-02-03 23:08:44 -0500

    Donation styling

--- PolitizenMobile/screens/Donate.js
+++ PolitizenMobile/screens/Donate.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, ScrollView, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Text,
+} from "react-native";
 
 export default function DonateScreen() {
+  const [number, onChangeNumber] = useState("");
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollVIew}>
-        <Text>Donate</Text>
+        <Text style={styles.pText}>Donate</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Donation here"
+          keyboardType="numeric"
+        />
       </ScrollView>
     </SafeAreaView>
   );
@@ -17,6 +31,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#dde4e5",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  input: {
+    height: 40,
+    width: 300,
+    marginTop: 90,
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+  },
+
+  pText: {
+    color: "#000",
+    textAlign: "center",
+    fontSize: 20,
+    padding: 10,
   },
 
   scrollView: {
