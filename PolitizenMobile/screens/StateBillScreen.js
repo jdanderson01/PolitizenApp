b42c3c6381c31ab30fc4e38114726437e734ae2b
harrypatiry <harrypatiry@gmail.com>
2023-03-10 13:08:30 -0500

    state legislation search

--- PolitizenMobile/screens/StateBillScreen.js
+++ PolitizenMobile/screens/StateBillScreen.js
@@ -7,9 +7,13 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  TextInput
 } from "react-native";
 import TopNav from "../components/Header";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+
+const API_KEY = "18c3b84f2e7b6ea9b7f13332187403e5";
 
 //so I dont have to check the doc again
 
@@ -17,45 +21,7 @@ import { useNavigation } from "@react-navigation/native";
 
 //getBillText template:https://api.legiscan.com/?key=APIKEY&op=getBillText&id=DOC_ID
 
-//initialize variable to hold the state bill data array
-const stateBillData = [
-  {
-    key: 1,
-    image: require("../assets/arizona-flag.png"),
-    title: "SB-1035",
-    component: "SB1035",
-  },
-  {
-    key: 2,
-    image: require("../assets/arizona-flag.png"),
-    title: "SB-1046",
-    component: "SB1046",
-  },
-  {
-    key: 3,
-    image: require("../assets/arizona-flag.png"),
-    title: "SB-1347",
-    component: "SB1347",
-  },
-  {
-    key: 4,
-    image: require("../assets/arizona-flag.png"),
-    title: "SB1339",
-    component: "SB1339",
-  },
-  {
-    key: 5,
-    image: require("../assets/arizona-flag.png"),
-    title: "SB-1511",
-    component: "SB1411",
-  },
-  {
-    key: 6,
-    image: require("../assets/arizona-flag.png"),
-    title: "SB-1042",
-    component: "SB1042",
-  },
-];
+//getMasterList template: https://api.legiscan.com/?key=APIKEY&op=getMasterList&state=STATE
 
 export default function StateBillScreen() {
   const navigation = useNavigation();
@@ -63,20 +29,62 @@ export default function StateBillScreen() {
     navigation.navigate(component);
   };
 
+  const [address, setAddress] = useState("");
+  const [bill, setBill] = useState([]);
+
+  const handleAddressChange = (text) => {
+    setAddress(text);
+  };
+
+  const handleSearch = () => {
+    const url = `https://api.legiscan.com/?key=${API_KEY}&op=getMasterList&state=${encodeURIComponent(address)}`;
+
+    //getting invalid state 
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+        setBill(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TopNav style={styles.topNav} />
+      <TopNav />
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Enter State"
+          value={address}
+          onChangeText={handleAddressChange}
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+          <Text style={styles.searchButtonText}>Search</Text>
+        </TouchableOpacity>
+      </View>
       <FlatList
-        numColumns={2}
-        data={stateBillData}
+        numColumns={1}
+        data={bill}
         renderItem={({ item }) => (
           <View style={styles.imgContainer}>
-            <TouchableOpacity onPress={() => handlePress(item.component)}>
-              <Image source={item.image} style={styles.image} />
-              <Text style={styles.title}>{item.title}</Text>
+            <TouchableOpacity onPress={() => handlePress(item.id)}>
+              <Image
+                source={{
+                  uri:
+                    item.photoUrl ||
+                    "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png",
+                }}
+                style={styles.image}
+              />
+              <Text style={styles.title}>{item}</Text>
             </TouchableOpacity>
           </View>
         )}
+        keyExtractor={(item) => item.id}
       />
     </SafeAreaView>
   );
