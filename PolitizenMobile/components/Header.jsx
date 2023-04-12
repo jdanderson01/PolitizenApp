6aa6612784ee0886efe5e92839ea6490abaae50b
Harry Patiry <60627616+harrypatiry@users.noreply.github.com>
2023-04-12 16:11:05 -0400

    fix header height change

--- PolitizenMobile/components/Header.jsx
+++ PolitizenMobile/components/Header.jsx
@@ -16,6 +16,7 @@ export default function TopNav() {
       containerStyle={{
         width: "100%",
         height: "15%",
+        maxHeight: "200px",
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#d5d5d5",
